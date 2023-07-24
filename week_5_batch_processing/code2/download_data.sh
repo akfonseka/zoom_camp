e1d33a19f6e9622b97d2dc2da9b2037a3c40c863
akfonseka <ak.fonseka97@gmail.com>
2023-07-24 14:22:47 +0000

    Added week 5'

---
+++ week_5_batch_processing/code2/download_data.sh
@@ -0,0 +1,29 @@
+
+set -e 
+
+TAXI_TYPE=$1
+YEAR=$2
+## https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2020-01.parquet
+## /yellow_tripdata_2020-01.parquet
+
+URL_PREFIX='https://d37ci6vzurychx.cloudfront.net/trip-data'
+
+for MONTH in {1..12}; do
+    FMONTH=`printf "%02d" ${MONTH}`
+
+    URL="${URL_PREFIX}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.parquet"
+
+    LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
+    LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.parquet.gz"
+    LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+    echo "downloading ${URL} to ${LOCAL_PATH}"
+    mkdir -p ${LOCAL_PREFIX}
+
+    wget ${URL} -O ${LOCAL_PATH}
+    
+    ## echo "compressing ${LOCAL_PATH}"
+    ## gzip ${LOCAL_PATH}
+
+done
+
